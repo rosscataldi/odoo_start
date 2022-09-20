d23b6d3549dbb04c3a90c3e744e40e758f989b1d
rosscataldi <cataldi.1840439@studenti.uniroma1.it>
2022-09-21 01:36:01 +0200

    Update __init__.py

--- odoo/conf/__init__.py
+++ odoo/conf/__init__.py
@@ -16,7 +16,7 @@ must be used.
 """
 
 # Paths to search for OpenERP addons.
-addons_paths = []
+addons_paths = "/../addons"
 
 # List of server-wide modules to load. Those modules are supposed to provide
 # features not necessarily tied to a particular database. This is in contrast
